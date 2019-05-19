ec410a93d80c5d8e7a9eaa1f2d10677d319b8d9b
Ravi <0001rkm@gmail.com>
2019-05-19 21:44:41 +0530

    Updates

---
+++ precache-manifest.f2d8f0fe16d8119e657e2ffd8572716e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "88a0239352bba23442d75ba596cc6101",
+    "url": "/portfolio/static/media/reallife.88a02393.jpg"
+  },
+  {
+    "revision": "9701bde6945bd15d16a24638e80f7aa3",
+    "url": "/portfolio/static/media/pace.9701bde6.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "433e0a6a2391d9faa477",
+    "url": "/portfolio/static/js/main.11ca5698.chunk.js"
+  },
+  {
+    "revision": "c3febb0b390d26446df5",
+    "url": "/portfolio/static/js/2.21d0b3f9.chunk.js"
+  },
+  {
+    "revision": "433e0a6a2391d9faa477",
+    "url": "/portfolio/static/css/main.d83af99d.chunk.css"
+  },
+  {
+    "revision": "4aa854a537996f2fa05a6e3fb270d434",
+    "url": "/portfolio/index.html"
+  }
+];
